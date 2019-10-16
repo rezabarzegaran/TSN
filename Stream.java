4aaaebd505f169fa5e256f9fb309e33e5d14caa4
Niklas Reusch <nreusch@web.de>
2019-10-16 15:57:14 +0200

    fixed wrong constant in Stream class. Started work on Niklas method

--- Stream.java
+++ Stream.java
@@ -13,7 +13,7 @@ public class Stream {
         offset = _offset;
         // 1 Byte takes 0.008us at 1000Mbits, 42bit header for ethernet packet. Round down.
         //Transmit_Time = (int) Math.ceil((Size + 42)*0.08);
-        Transmit_Time = (int) Math.ceil((Size)*0.08);
+        Transmit_Time = (int) Math.ceil((Size)*0.008);
         //Transmit_Time = (int) Math.ceil((Size)*0.02);
         routingList = new ArrayList<String>();
         CroutingList = new ArrayList<String>();
